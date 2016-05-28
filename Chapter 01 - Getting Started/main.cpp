0fd5d7eb0596cedc310ff7e214e3839c6dfac5fe
Diego Martinez <dmartinezgarcia@outlook.com>
2016-05-28 16:57:04 +0100

    Auto format files

--- Chapter 01 - Getting Started/main.cpp
+++ Chapter 01 - Getting Started/main.cpp
@@ -30,32 +30,33 @@ void ExerciseNum25 (void);
 
 typedef void (*Exercise) (void);
 // Fill the array as you add exercises.
-Exercise Exercises[] = {
-		ExerciseNum1,
-		ExerciseNum2,
-		ExerciseNum3,
-		ExerciseNum4,
-		ExerciseNum5,
-		ExerciseNum6,
-		ExerciseNum7,
-		ExerciseNum8,
-		ExerciseNum9,
-		ExerciseNum10,
-		ExerciseNum11,
-		ExerciseNum12,
-		ExerciseNum13,
-		ExerciseNum14,
-		ExerciseNum15,
-		ExerciseNum16,
-		ExerciseNum17,
-		ExerciseNum18,
-		ExerciseNum19,
-		ExerciseNum20,
-		ExerciseNum21,
-		ExerciseNum22,
-		ExerciseNum23,
-		ExerciseNum24,
-		ExerciseNum25
+Exercise Exercises[] =
+{
+    ExerciseNum1,
+    ExerciseNum2,
+    ExerciseNum3,
+    ExerciseNum4,
+    ExerciseNum5,
+    ExerciseNum6,
+    ExerciseNum7,
+    ExerciseNum8,
+    ExerciseNum9,
+    ExerciseNum10,
+    ExerciseNum11,
+    ExerciseNum12,
+    ExerciseNum13,
+    ExerciseNum14,
+    ExerciseNum15,
+    ExerciseNum16,
+    ExerciseNum17,
+    ExerciseNum18,
+    ExerciseNum19,
+    ExerciseNum20,
+    ExerciseNum21,
+    ExerciseNum22,
+    ExerciseNum23,
+    ExerciseNum24,
+    ExerciseNum25
 };
 
 #include <stdio.h>
@@ -71,7 +72,7 @@ int returnValue = 0;
 
 void ExerciseNum1 (void)
 {
-	returnValue = 0;
+    returnValue = 0;
 }
 
 /********************************************//**
@@ -79,10 +80,9 @@ void ExerciseNum1 (void)
  *
  *  Change the program to return -1. Recompile and rerun your program to see how your system treats a failure indicator from main.
  ***********************************************/
-
 void ExerciseNum2 (void)
 {
-	returnValue = -1;
+    returnValue = -1;
 }
 
 /********************************************//**
@@ -90,10 +90,9 @@ void ExerciseNum2 (void)
  *
  *  Write a program to print Hello, World on the standard output.
  ***********************************************/
-
 void ExerciseNum3 (void)
 {
-	std::cout << "Hello, world" << std::endl;
+    std::cout << "Hello, world" << std::endl;
 }
 
 /********************************************//**
@@ -101,16 +100,15 @@ void ExerciseNum3 (void)
  *
  *  Our program used the addition operator, +, to add two numbers. Write a program that uses the multiplication operator, *, to print the product instead.
  ***********************************************/
-
 void ExerciseNum4 (void)
 {
-	int v1, v2;
+    int v1, v2;
 
-	std::cout << "Please input the first operand: " << std::endl;
-	std::cin >> v1;
-	std::cout << "Please input the second operand: " << std::endl;
-	std::cin >> v2;
-	std::cout << "The result is: " << v1 * v2 << std::endl;
+    std::cout << "Please input the first operand: " << std::endl;
+    std::cin >> v1;
+    std::cout << "Please input the second operand: " << std::endl;
+    std::cin >> v2;
+    std::cout << "The result is: " << v1 * v2 << std::endl;
 }
 
 /********************************************//**
@@ -118,16 +116,15 @@ void ExerciseNum4 (void)
  *
  *  We wrote the output in one large statement. Rewrite the program to use a separate statement to print each operand.
  ***********************************************/
-
 void ExerciseNum5 (void)
 {
-	std::cout << "The sum of ";
-	std::cout << 1;
-	std::cout << " and ";
-	std::cout << 10;
-	std::cout << " is ";
-	std::cout << 1 + 10;
-	std::cout << std::endl;
+    std::cout << "The sum of ";
+    std::cout << 1;
+    std::cout << " and ";
+    std::cout << 10;
+    std::cout << " is ";
+    std::cout << 1 + 10;
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -136,15 +133,14 @@ void ExerciseNum5 (void)
  *  Explain whether the following program fragment is legal. If the program is legal, what does it do? If the program is not legal, why not? How would you fix it?
  *
  *  std::cout << "The sum of " << v1;
- *  		  << " and " << v2;
- *  		  << " is " << v1 + v2 << std::endl;
+ *            << " and " << v2;
+ *            << " is " << v1 + v2 << std::endl;
  ***********************************************/
-
 void ExerciseNum6 (void)
 {
-	int v1 = 10, v2 = 20;
+    int v1 = 10, v2 = 20;
 
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
 }
 
 /********************************************//**
@@ -152,10 +148,9 @@ void ExerciseNum6 (void)
  *
  *  Compile a program that has incorrectly nested comments.
  ***********************************************/
-
 void ExerciseNum7 (void)
 {
-	/* Easy to do. */
+    /* Easy to do. */
 }
 
 /********************************************//**
@@ -167,14 +162,13 @@ void ExerciseNum7 (void)
  *
  *  After you've predicted what will happen, test your answers by compiling a program with each of these statements. Correct any errors you encounter.
  ***********************************************/
-
 void ExerciseNum8 (void)
 {
-	/* This are the correct statements. */
-	std::cout << "/*";
-	std::cout << "*/";
-	std::cout << "/* */ */";
-	std::cout << "/* */ /* /* */";
+    /* This are the correct statements. */
+    std::cout << "/*";
+    std::cout << "*/";
+    std::cout << "/* */ */";
+    std::cout << "/* */ /* /* */";
 }
 
 /********************************************//**
@@ -182,15 +176,16 @@ void ExerciseNum8 (void)
  *
  *  Write a program that uses a while to sum the numbers from 50 to 100.
  ***********************************************/
-
 void ExerciseNum9 (void)
 {
-	int sum = 0, i = 50;
+    int sum = 0, i = 50;
 
-	while (i <= 100)
-		sum += i++;
+    while (i <= 100)
+    {
+        sum += i++;
+    }
 
-	std::cout << "Total sum: " << sum << std::endl;
+    std::cout << "Total sum: " << sum << std::endl;
 }
 
 /********************************************//**
@@ -200,11 +195,14 @@ void ExerciseNum9 (void)
  ***********************************************/
 void ExerciseNum10 (void)
 {
-	int i = 10;
+    int i = 10;
 
-	while (i != 0)
-		std::cout << i-- << "\r";
-	std::cout << std::endl;
+    while (i != 0)
+    {
+        std::cout << i-- << "\r";
+    }
+
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -214,13 +212,16 @@ void ExerciseNum10 (void)
  ***********************************************/
 void ExerciseNum11 (void)
 {
-	int i, a;
+    int i, a;
 
-	std::cin >> i >> a;
+    std::cin >> i >> a;
 
-	while (++i < a)
-		std::cout << i << " ";
-	std::cout << std::endl;
+    while (++i < a)
+    {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -230,10 +231,14 @@ void ExerciseNum11 (void)
  ***********************************************/
 void ExerciseNum12 (void)
 {
-	int sum = 0, i;
-	for (i = -100; i <= 100; i++)
-		sum += i;
-	std::cout << i << std::endl;
+    int sum = 0, i;
+
+    for (i = -100; i <= 100; i++)
+    {
+        sum += i;
+    }
+
+    std::cout << i << std::endl;
 }
 
 /********************************************//**
@@ -243,18 +248,18 @@ void ExerciseNum12 (void)
  ***********************************************/
 void ExerciseNum13 (void)
 {
-	int sum, i;
+    int sum, i;
 
-	/* First program. */
-	for (i = 50, sum = 0; i <= 100; sum += i++);
+    /* First program. */
+    for (i = 50, sum = 0; i <= 100; sum += i++);
 
-	std::cout << "Total sum: " << sum << std::endl;
+    std::cout << "Total sum: " << sum << std::endl;
 
-	/* Second program. */
-	std::cin >> i >> sum;
+    /* Second program. */
+    std::cin >> i >> sum;
 
-	for (; ++i < sum; std::cout << i << " ");
-	std::cout << std::endl;
+    for (; ++i < sum; std::cout << i << " ");
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -280,61 +285,76 @@ void ExerciseNum15 (void)
 /********************************************//**
  *  Exercise 1.16
  *
- *	Write your version of a program that prints the sum of a set of integers read from cin.
+ *  Write your version of a program that prints the sum of a set of integers read from cin.
  ***********************************************/
 void ExerciseNum16 (void)
 {
-	int a, sum = 0;
+    int a, sum = 0;
 
-	while (std::cin >> a)
-		sum += a;
-	std::cout << "Total sum: " << sum << std::endl;
+    while (std::cin >> a)
+    {
+        sum += a;
+    }
+
+    std::cout << "Total sum: " << sum << std::endl;
 }
 
 /********************************************//**
  *  Exercise 1.17
  *
- *	What happens in the program presented in this section if the input values are all equal? What if there are no duplicated values?
+ *  What happens in the program presented in this section if the input values are all equal? What if there are no duplicated values?
  ***********************************************/
 void ExerciseNum17 (void)
 {
-	int currVal = 0, val = 0;
-	if (std::cin >> currVal) {
-		int cnt = 1;
-		while (std::cin >> val) {
-			if (val == currVal)
-				++cnt;
-			else {
-				std::cout << currVal << " occurs " << cnt << " times" << std::endl;
-				currVal = val;
-				cnt = 1;
-			}
-		}
-		std::cout << currVal << " occurs " << cnt << " times" << std::endl;
-	}
+    int currVal = 0, val = 0;
+
+    if (std::cin >> currVal)
+    {
+        int cnt = 1;
+        while (std::cin >> val)
+        {
+            if (val == currVal)
+            {
+                ++cnt;
+            }
+            else
+            {
+                std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+                currVal = val;
+                cnt = 1;
+            }
+        }
+        std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+    }
 }
 
 /********************************************//**
  *  Exercise 1.18
  *
- *	Compile and run the program from this section giving it only equal values as input. Run it again giving it values in which no number is repeated.
+ *  Compile and run the program from this section giving it only equal values as input. Run it again giving it values in which no number is repeated.
  ***********************************************/
 void ExerciseNum18 (void)
 {
-	int currVal = 0, val = 0;
-	if (std::cin >> currVal) {
-		int cnt = 1;
-		while (std::cin >> val) {
-			if (val == currVal)
-				++cnt;
-			else {
-				std::cout << currVal << " occurs " << cnt << " times" << std::endl;
-				currVal = val;
-				cnt = 1;
-			}
-		}
-		std::cout << currVal << " occurs " << cnt << " times" << std::endl;
-	}
+    int currVal = 0, val = 0;
+
+    if (std::cin >> currVal)
+    {
+        int cnt = 1;
+        while (std::cin >> val)
+        {
+            if (val == currVal)
+            {
+                ++cnt;
+            }
+            else
+            {
+                std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+                currVal = val;
+                cnt = 1;
+            }
+        }
+        std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+    }
 }
 
 /********************************************//**
@@ -344,18 +364,19 @@ void ExerciseNum18 (void)
  ***********************************************/
 void ExerciseNum19 (void)
 {
-	int a, b;
+    int a, b;
 
-	/* Second program. */
-	std::cin >> a >> b;
-	if (a > b) {
-		a = a ^ b;
-		b = a ^ b;
-		a = a ^ b;
-	}
+    /* Second program. */
+    std::cin >> a >> b;
+    if (a > b)
+    {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
 
-	for (; ++a < b; std::cout << a << " ");
-	std::cout << std::endl;
+    for (; ++a < b; std::cout << a << " ");
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -365,11 +386,13 @@ void ExerciseNum19 (void)
  ***********************************************/
 void ExerciseNum20 (void)
 {
-	Sales_item book;
+    Sales_item book;
 
-	std::cout << "Enter sale items in the following format: <ISBN - string> <Units - int> <Value - float>\r\n" << std::endl;
-	while (std::cin >> book)
-		std::cout << book << std::endl;
+    std::cout << "Enter sale items in the following format: <ISBN - string> <Units - int> <Value - float>\r\n" << std::endl;
+    while (std::cin >> book)
+    {
+        std::cout << book << std::endl;
+    }
 }
 
 /********************************************//**
@@ -379,15 +402,19 @@ void ExerciseNum20 (void)
  ***********************************************/
 void ExerciseNum21 (void)
 {
-	Sales_item book1, book2;
+    Sales_item book1, book2;
 
-	std::cout << "Enter two books that have the same ISBN: ";
-	std::cin >> book1 >> book2;
-	if (book1.isbn() == book2.isbn())
-		std::cout << "\nThe books sum is " << book1 + book2;
-	else
-		std::cout << "\nThe books don't have the same ISBN";
-	std::cout << std::endl;
+    std::cout << "Enter two books that have the same ISBN: ";
+    std::cin >> book1 >> book2;
+    if (book1.isbn() == book2.isbn())
+    {
+        std::cout << "\nThe books sum is " << book1 + book2;
+    }
+    else
+    {
+        std::cout << "\nThe books don't have the same ISBN";
+    }
+    std::cout << std::endl;
 }
 
 /********************************************//**
@@ -397,18 +424,24 @@ void ExerciseNum21 (void)
  ***********************************************/
 void ExerciseNum22 (void)
 {
-	Sales_item book1, book2;
+    Sales_item book1, book2;
 
-	while (std::cin >> book1)
-	{
-		if (book2.isbn() == "")
-			book2 = book1;
-		else if (book2.isbn() != book1.isbn()) {
-			std::cout << "Transaction details: " << book2 << std::endl;
-			book2 = book1;
-		} else
-			book2 += book1;
-	}
+    while (std::cin >> book1)
+    {
+        if (book2.isbn() == "")
+        {
+            book2 = book1;
+        }
+        else if (book2.isbn() != book1.isbn())
+        {
+            std::cout << "Transaction details: " << book2 << std::endl;
+            book2 = book1;
+        }
+        else
+        {
+            book2 += book1;
+        }
+    }
 }
 
 /********************************************//**
@@ -418,17 +451,19 @@ void ExerciseNum22 (void)
  ***********************************************/
 void ExerciseNum23 (void)
 {
-	int nTransactions = 0;
-	Sales_item book1, book2;
+    int nTransactions = 0;
+    Sales_item book1, book2;
 
-	while (std::cin >> book1) {
-		if ((book2.isbn() != book1.isbn()) && (book2.isbn() != "")) {
-			std::cout << nTransactions << " transactions for ISBN: " << book1.isbn() << std::endl;
-			nTransactions = 0;
-		}
-		book2 = book1;
-		nTransactions++;
-	}
+    while (std::cin >> book1)
+    {
+        if ((book2.isbn() != book1.isbn()) && (book2.isbn() != ""))
+        {
+            std::cout << nTransactions << " transactions for ISBN: " << book1.isbn() << std::endl;
+            nTransactions = 0;
+        }
+        book2 = book1;
+        nTransactions++;
+    }
 }
 
 /********************************************//**
@@ -438,17 +473,19 @@ void ExerciseNum23 (void)
  ***********************************************/
 void ExerciseNum24 (void)
 {
-	int nTransactions = 0;
-	Sales_item book1, book2;
+    int nTransactions = 0;
+    Sales_item book1, book2;
 
-	while (std::cin >> book1) {
-		if ((book2.isbn() != book1.isbn()) && (book2.isbn() != "")) {
-			std::cout << nTransactions << " transactions for ISBN: " << book1.isbn() << std::endl;
-			nTransactions = 0;
-		}
-		book2 = book1;
-		nTransactions++;
-	}
+    while (std::cin >> book1)
+    {
+        if ((book2.isbn() != book1.isbn()) && (book2.isbn() != ""))
+        {
+            std::cout << nTransactions << " transactions for ISBN: " << book1.isbn() << std::endl;
+            nTransactions = 0;
+        }
+        book2 = book1;
+        nTransactions++;
+    }
 }
 
 /********************************************//**
@@ -458,39 +495,48 @@ void ExerciseNum24 (void)
  ***********************************************/
 void ExerciseNum25 (void)
 {
-	Sales_item total;
-	if (std::cin >> total) {
-		Sales_item trans;
-		while (std::cin >> trans) {
-			if (total.isbn() == trans.isbn()) {
-				total += trans;
-			} else {
-				std::cout << total << std::endl;
-				total = trans;
-			}
-		}
-		std::cout << total << std::endl;
-	} else {
-		std::cerr << "No data?!" << std::endl;
-	}
+    Sales_item total;
+    if (std::cin >> total)
+    {
+        Sales_item trans;
+        while (std::cin >> trans)
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total += trans;
+            }
+            else
+            {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << total << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+    }
 }
 
 /********************************************//**
  *  Main function do not modify.
  ***********************************************/
-
 int main (void)
 {
-	int TotalNumberOfExercises = sizeof(Exercises) / sizeof (Exercise);
+    int TotalNumberOfExercises = sizeof(Exercises) / sizeof (Exercise);
 
-	if (ExerciseNum >= 1 && ExerciseNum <= TotalNumberOfExercises)
-	{
-		printf("Exercise Number %02d\n === S T A R T ===\n\n", ExerciseNum);
-		Exercises[ExerciseNum - 1]();
-		printf("\n\n ===== E N D =====\nExercise Number %02d\n", ExerciseNum);
-	}
-	else
-		printf("No exercise executed. ExerciseNum is not a valid value.\n");
-	return returnValue;
+    if (ExerciseNum >= 1 && ExerciseNum <= TotalNumberOfExercises)
+    {
+        printf("Exercise Number %02d\n === S T A R T ===\n\n", ExerciseNum);
+        Exercises[ExerciseNum - 1]();
+        printf("\n\n ===== E N D =====\nExercise Number %02d\n", ExerciseNum);
+    }
+    else
+    {
+        printf("No exercise executed. ExerciseNum is not a valid value.\n");
+    }
+
+    return returnValue;
 }
 
--- Template/main.cpp
+++ Template/main.cpp
@@ -7,9 +7,10 @@ void ExerciseNum2 (void);
 
 typedef void (*Exercise) (void);
 // Fill the array as you add exercises.
-Exercise Exercises[] = {
-		ExerciseNum1,
-		ExerciseNum2
+Exercise Exercises[] =
+{
+    ExerciseNum1,
+    ExerciseNum2
 };
 
 #include <stdio.h>
@@ -17,37 +18,37 @@ Exercise Exercises[] = {
 /********************************************//**
  *  This is exercise 1 explanation.
  ***********************************************/
-
 void ExerciseNum1 (void)
 {
-	/* This is exercise 1. */
+    /* This is exercise 1. */
 }
 
 /********************************************//**
  *  This is exercise 2 explanation.
  ***********************************************/
-
 void ExerciseNum2 (void)
 {
-	/* This is exercise 2. */
+    /* This is exercise 2. */
 }
 
 /********************************************//**
  *  Main function do not modify.
  ***********************************************/
-
 int main (void)
 {
-	int TotalNumberOfExercises = sizeof(Exercises) / sizeof (Exercise);
+    int TotalNumberOfExercises = sizeof(Exercises) / sizeof (Exercise);
 
-	if (ExerciseNum >= 1 && ExerciseNum <= TotalNumberOfExercises)
-	{
-		printf("Exercise Number %02d\n === S T A R T ===\n\n", ExerciseNum);
-		Exercises[ExerciseNum - 1]();
-		printf("\n\n ===== E N D =====\nExercise Number %02d\n", ExerciseNum);
-	}
-	else
-		printf("No exercise executed. ExerciseNum is not a valid value.\n");
-	return 0;
+    if (ExerciseNum >= 1 && ExerciseNum <= TotalNumberOfExercises)
+    {
+        printf("Exercise Number %02d\n === S T A R T ===\n\n", ExerciseNum);
+        Exercises[ExerciseNum - 1]();
+        printf("\n\n ===== E N D =====\nExercise Number %02d\n", ExerciseNum);
+    }
+    else
+    {
+        printf("No exercise executed. ExerciseNum is not a valid value.\n");
+    }
+
+    return 0;
 }
 
